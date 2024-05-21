39c4068914801432c72efce3c44191cc28bb2e96
yjxu <yxu349@aucklanduni.ac.nz>
2024-05-21 22:04:55 +1200

    1. Completed the Login component and InputBar component 2. Worked on the validation in front-end when registering

--- frontend/src/lib/components/InputBar.svelte
+++ frontend/src/lib/components/InputBar.svelte
@@ -1,2 +1,113 @@
-// This component is for the input bar that is used to input the text for username, password, etc. It defines the layout of the input bar. It is used in the login page and signup page.
-// Four properties (from parent to the component): label text, input type, default value and input validation function
+<script>
+  /** This component is for the input bar that is used to input the text for username, password, etc.
+   * It defines the layout of the input bar. It is used in the login page and signup page.
+   * Four properties (from parent to the component): label text, input type, default value and input validation function
+   **/
+  export let label = "";
+  export let type = "text";
+  export let value = "";
+  export let placeholder = "";
+  export let validate = (value) => {
+    return { result: true, errorMsg: "" };
+  };
+  let validateResult = true;
+  let errorMsg = "";
+  function checkValue() {
+    const validation = validate(value);
+    errorMsg = validation.errorMsg;
+    validateResult = validation.result;
+  }
+  function clearError() {
+    errorMsg = "";
+    validateResult = true;
+  }
+</script>
+
+<div class="input-bar">
+  <label for={label}>{label}</label>
+  <div class="input-section">
+    {#if type === "password"}
+      <input
+        class:active={!validateResult}
+        id={label}
+        type="password"
+        {placeholder}
+        bind:value
+        on:blur={checkValue}
+        on:focus={clearError}
+      />
+    {:else if type === "email"}
+      <input
+        class:active={!validateResult}
+        id={label}
+        type="email"
+        {placeholder}
+        bind:value
+        on:blur={checkValue}
+        on:focus={clearError}
+      />
+    {:else if type === "date"}
+      <input
+        class:active={!validateResult}
+        id={label}
+        type="date"
+        {placeholder}
+        bind:value
+        on:blur={checkValue}
+        on:focus={clearError}
+      />
+    {:else}
+      <input
+        class:active={!validateResult}
+        id={label}
+        type="text"
+        {placeholder}
+        bind:value
+        on:blur={checkValue}
+        on:focus={clearError}
+      />
+    {/if}
+    {#if !validateResult}
+      <p class="error">{errorMsg}</p>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .active {
+    border: 1px solid rgba(255, 0, 0, 0.837);
+  }
+  .input-section {
+    display: flex;
+    align-items: center;
+  }
+  .input-bar {
+    width: 350px;
+    position: relative;
+  }
+  .error {
+    color: rgba(255, 0, 0, 0.837);
+    position: absolute;
+    bottom: -1.3em;
+    right: 0px;
+    font-size: 0.8em;
+  }
+  label {
+    display: block;
+    margin-top: 1em;
+    color:#808080;
+  }
+  input {
+    outline: none;
+    width: 350px;
+    padding: 1em 0.5em;
+    margin-top: 0.5em;
+    margin-bottom: 1em;
+    border: 1px solid #ddd;
+    background-color: #ddd;
+    color: #606060;
+    &::placeholder {
+      color: #909090;
+    }
+  }
+</style>
---
+++ frontend/src/lib/components/Login.svelte
@@ -0,0 +1,89 @@
+<script>
+  import { goto } from "$app/navigation";
+  import InputBar from "../../lib/components/InputBar.svelte";
+  let userName;
+  $: console.log(userName);
+  let password;
+  $: console.log(password);
+  function processLogin() {
+    console.log("Processing login");
+    console.log(userName);
+  }
+  function validateUserName(value) {
+    if (value.length < 3) {
+        return {result:false, errorMsg:"Username must be at least 3 characters long"}
+    }
+    return { result: true, errorMsg: "" };
+  }
+  function validatePassword(value) {
+    if (value.length < 8) {
+      return {result:false, errorMsg:"Password must be at least 8 characters long"}
+    }
+    return { result: true, errorMsg: "" };
+    }
+</script>
+
+<div class="login-container">
+    <strong>Welcome back!</strong>
+    <p>Login using an existing account below</p>
+  <div class="login-bar">
+    <InputBar
+      label="USERNAME:"
+      type="text"
+      placeholder="Enter your username"
+      validate={validateUserName}
+      bind:value={userName}
+    />
+
+    <InputBar
+      label="PASSWORD:"
+      type="password"
+      placeholder="Password (8+ characters)"
+      validate={validatePassword}
+      bind:value={password}
+    />
+
+    <button on:click={processLogin}>LOGIN</button>
+  </div>
+</div>
+
+<style>
+    .login-container>strong {
+        font-size: 3em;
+        margin-bottom: 10px;
+        color:#505050;
+    }
+    .login-container>p {
+        font-size: 1.3em;
+        margin-bottom: 20px;
+        color:#505050;
+    }  
+.login-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: 10% 2% 20% 10%;
+        border: 1px solid #505050;
+        padding: 80px 50px 50px 40px;
+    }
+  .login-bar {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10%;
+  }
+  button {
+    margin-top: 20px;
+    padding: 10px;
+    background-color: #bbb;
+    color: white;
+    border: none;
+    cursor: pointer;
+    width: 120px;
+  }
+  button:hover {
+    background-color: #202020;
+  }
+</style>
