2ebcb52ce961c12dc21a8ce2457f6fffe9a0c8aa
yjxu <yxu349@aucklanduni.ac.nz>
2024-05-21 21:11:37 +1200

    Modified the position of comments

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,3 +1,7 @@
-// ---routes/login/+page.svelte
-// It is a svelte file that contains the login page.
-// API: POST /api/users/login if the user login successfully, then the user will be redirected to the home page.
+<script>
+  /** ---routes/login/+page.svelte
+   * It is a svelte file that contains the login page.
+   * API: POST /api/users/login if the user login successfully, then the user will be redirected to the home page.
+   **/
+</script>
+
