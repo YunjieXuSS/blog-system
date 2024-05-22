ff9902436dfea7f728749690352772b2b3ca9d0a
jade <jber689@aucklanduni.ac.nz>
2024-05-22 13:11:24 +1200

    added basic placeholder text to user info component

--- frontend/src/lib/components/UserInfo.svelte
+++ frontend/src/lib/components/UserInfo.svelte
@@ -1,1 +1,25 @@
 <!-- Displays the user's information (username, name, avatar, description) on their profile. -->
+
+<script>
+    export let username = "username";
+    export let firstName = "fName";
+    export let lastName = "lName";
+    export let avatarURL = "avatarURL.jpg";
+    export let description = "description";
+</script>
+
+<div class="profileAvatar">
+    <img src="{avatarURL}" alt="User avatar" />
+</div>
+
+<div class="profileInfo">
+    <h2>{username}</h2>
+    <h5>{firstName} {lastName}</h5>
+    <p>{description}</p>
+</div>
+
+<style>
+    * {
+        border: 1px solid black;
+    }
+</style>
