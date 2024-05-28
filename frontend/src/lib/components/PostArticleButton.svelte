8f8f3804ffd1f68f11fc5911001f33db0fce610d
Lunachenyuee <yche631@aucklanduni.ac.nz>
2024-05-28 12:00:36 +1200

    adding funticon to PostArticleButton Component

--- frontend/src/lib/components/PostArticleButton.svelte
+++ frontend/src/lib/components/PostArticleButton.svelte
@@ -2,10 +2,34 @@
 
 <script>
   import ButtonText from "$lib/components/ButtonText.svelte";
+  import { goto } from "$app/navigation";
+  import { refreshPage } from "../js/utils.js";
+  export let data;
+
+  async function handlePostNewArticle() {
+    console.log("post new article");
+    const isLoggedIn = data.isLoggedIn;
+
+    if (isLoggedIn) {
+      goto("/articles/create", { replaceState: true });
+      await refreshPage();
+    } else {
+      goto("/login", { replaceState: true });
+      await refreshPage();
+    }
+  }
+
 </script>
 
 <div class="button-container">
-  <ButtonText buttonLabel="✚ New article" buttonPath="/articles/create" bckgColour="#F5E8DD" txtColour="#B5C0D0" borderRadius="4px 0 0 4px"/>
+  <ButtonText
+    buttonLabel="✚ New article"
+    buttonPath="/articles/create"
+    bckgColour="#F5E8DD"
+    txtColour="#B5C0D0"
+    borderRadius="4px 0 0 4px"
+    buttonFunction={handlePostNewArticle}
+  />
 </div>
 
 <style>
