df4d9efbfd12c83ea6769db49f08d4acbe913a96
yjxu <yxu349@aucklanduni.ac.nz>
2024-05-29 17:18:55 +1200

    Fixed bugs

--- frontend/src/lib/components/NavigationBar.svelte
+++ frontend/src/lib/components/NavigationBar.svelte
@@ -130,7 +130,7 @@
       <li>
         <a
           href="/profile/{data.user.userName}"
-          class:active={path.includes(`/profile/${data.user.userName}`)}>Profile</a
+          class:active={path.startsWith(`/profile/${data.user.userName}`)}>Profile</a
         >
       </li>
     {/if}
